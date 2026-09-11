986ea77f3e8ee4cf805083d2fb38ae6a0d2d667d
Jasurbek <jasurbek@Jasurbeks-MacBook-Pro.local>
2026-09-11 19:20:38 +0900

    Trainjs UpdateTAsk

--- train.js
+++ train.js
@@ -1,62 +1,16 @@
-// D - Task
-
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
-
-  qoldiq() {
-    let vaqt = new Date().toLocaleTimeString("uz-UZ", {
-      hour: "2-digit",
-      minute: "2-digit"
-    });
-
-    return `Hozir ${vaqt}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`;
-  }
-
-  sotish(mahsulot, soni) {
-    let vaqt = new Date().toLocaleTimeString("uz-UZ", {
-      hour: "2-digit",
-      minute: "2-digit"
-    });
-
-    if (mahsulot === "non") {
-      this.non -= soni;
-    } else if (mahsulot === "lagmon") {
-      this.lagmon -= soni;
-    } else if (mahsulot === "cola") {
-      this.cola -= soni;
+// F - Task
+function findDoublers(str) {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if (str[i] === str[j]) {
+                return true;
+            }
+        }
     }
 
-    console.log(`Sotish amalga oshdi. Vaqt: ${vaqt}`);
-  }
-
-  qabul(mahsulot, soni) {
-    let vaqt = new Date().toLocaleTimeString("uz-UZ", {
-      hour: "2-digit",
-      minute: "2-digit"
-    });
-
-    if (mahsulot === "non") {
-      this.non += soni;
-    } else if (mahsulot === "lagmon") {
-      this.lagmon += soni;
-    } else if (mahsulot === "cola") {
-      this.cola += soni;
-    }
-
-    console.log(`Qabul amalga oshdi. Vaqt: ${vaqt}`);
-  }
+    return false;
 }
 
-
-const shop = new Shop(4, 5, 2);
-
-console.log(shop.qoldiq());
-
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
-
-console.log(shop.qoldiq());
+console.log(findDoublers("hello")); // true
+console.log(findDoublers("abc"));   // false
+console.log(findDoublers("apple"));  // true
